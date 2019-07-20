0ffb1dac2c9d6b914fe5de38f1a2a8e8d5e616ba
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-07-20 10:52:35 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
 
   console.log(`Logged in as ${client.user.username}!`);
 
-  client.user.setActivity('LorD Kaname made by Anant', {type: "streaming"})
+  client.user.setActivity('made by @ZEUS (I'M HURT)™ٴ#7111', {type: "streaming"})
 
 console.log(`${client.user.tag} running on ${client.guilds.size} guilds with ${client.users.size} users.`);
 
@@ -63,11 +63,11 @@ if(cmd === `help`) {
     .setAuthor(message.author.username)
     .setColor('RANDOM')
     .setDescription("Bot Details")
-    .addField("Bot Owner", "Anant", true)
+    .addField("Bot Owner", "@ZEUS (I'M HURT)™ٴ#7111", true)
     .addField("Language", "Javascript Hosted on Heruko 24 hours", true)
     .addField("Features", "Rainbow Roles", true)
-    .addField("Invite me", "Click [Here](https://discordapp.com/oauth2/authorize?client_id=568434092753027074&permissions=8&scope=bot)", true)
-    .addField("Thanks for making me;D", "One GhostRider", true)
+    .addField("Invite me", "Click [Here](https://discordapp.com/oauth2/authorize?client_id= your bot client id &permissions=8&scope=bot)", true)
+    .addField("Thanks for making me;D", "@ZEUS (I'M HURT)™ٴ#7111", true)
     .setFooter(client.user.tag)
     .setTimestamp();
     message.channel.send(embed)
